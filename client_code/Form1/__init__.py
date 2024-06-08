4e5864d0c04dd8b68de55ba0439c72d9f9efdef8
jacob.thompson@wiwasolvet.ca <jacob.thompson@wiwasolvet.ca>
2024-06-08 23:03:50 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 
 class Form1(Form1Template):
